618c1cc7cf2b212c0917de8a63ca3363e60201fd
Glitch (greet-bot) <none>
2020-08-31 22:49:45 +0000

    💎👴 Checkpoint ./index.js:21553861/1564

--- index.js
+++ index.js
@@ -7,7 +7,62 @@ const app = new App({
 
 // Listens to incoming messages that contain "hello"
 app.event('app_home_opened', ({ event, say }) => {  
-    say(`Hello world, <@${event.user}>!`);
+    say(`Hello I am your slack monkey, <@${event.user}>!`);
+});
+app.event('app_home_opened', async ({ event, context }) => {
+  try {
+    /* view.publish is the method that your app uses to push a view to the Home tab */
+    const result = await app.client.views.publish({
+
+      /* retrieves your xoxb token from context */
+      token: context.botToken,
+
+      /* the user that opened your app's app home */
+      user_id: event.user,
+
+      /* the view object that appears in the app home*/
+      view: {
+        type: 'home',
+        callback_id: 'home_view',
+
+        /* body of the view */
+        blocks: [
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": "*Whats up, I am your personal robot monkey!!!* :monkey::banana:"
+            }
+          },
+          {
+            "type": "divider"
+          },
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": "Helpng you to make inform decisions, about your vote in Metro Atlanta! "
+            }
+          },
+          {
+            "type": "actions",
+            "elements": [
+              {
+                "type": "button",
+                "text": {
+                  "type": "plain_text",
+                  "text": "Click me!"
+                }
+              }
+            ]
+          }
+        ]
+      }
+    });
+  }
+  catch (error) {
+    console.error(error);
+  }
 });
 (async () => {
   // Start your app
