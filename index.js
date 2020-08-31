373b1a522b2bf6c52702d1b1aeafcb0f75ec6fce
Glitch (greet-bot) <none>
2020-08-31 22:37:46 +0000

    💎🍦 Checkpoint ./index.js:21553861/5655

--- index.js
+++ index.js
@@ -1,42 +1,17 @@
 const { App } = require('@slack/bolt');
-const store = require('./store');
 
 const app = new App({
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
-  token: process.env.SLACK_BOT_TOKEN
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
 // Listens to incoming messages that contain "hello"
-app.message('hello', async ({ message, say }) => {
-  // say() sends a message to the channel where the event was triggered
-  await say(`Hey there <@${message.user}>!`);
+app.event('app_home_opened', ({ event, say }) => {  
+    say(`Hello world, <@${event.user}>!`);
 });
-
 (async () => {
   // Start your app
   await app.start(process.env.PORT || 3000);
 
-/*app.event('app_home_opened', ({ event, say }) => {  
-  // Look up the user from DB
-  let user = store.getUser(event.user);
-  
-  if(!user) {
-    user = {
-      user: event.user,
-      channel: event.channel
-    };
-    store.addUser(user);
-    
-    say(`Hello world, and welcome <@${event.user}>!`);
-  } else {
-    say('Hi again!');
-  }
-});*/
-
-
-// Start your app
-(async () => {
-  await app.start(process.env.PORT || 3000);
   console.log('⚡️ Bolt app is running!');
 })();
-
